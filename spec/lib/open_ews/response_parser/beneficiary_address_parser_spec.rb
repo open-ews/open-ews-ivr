e040a3598762a61f1ad994ece85ab5e6607c058c
David Wilkie <dwilkie@gmail.com>
2026-01-26 13:42:39 +0700

    Improve beneficiary address parser (#135)

--- spec/lib/open_ews/response_parser/beneficiary_address_parser_spec.rb
+++ spec/lib/open_ews/response_parser/beneficiary_address_parser_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 module OpenEWS
   module ResponseParser
     RSpec.describe BeneficiaryAddressParser do
-      it "parses a beneficiary response" do
+      it "parses a beneficiary address response" do
         parser = BeneficiaryAddressParser.new
 
         beneficiary_address = parser.parse(file_fixture("lib/open_ews/beneficiary_address.json").read)
@@ -16,6 +16,24 @@ module OpenEWS
           administrative_division_level_3_name: "Banteay Neang",
           administrative_division_level_4_code: "01020101",
           administrative_division_level_4_name: "Ou Thum",
+        )
+      end
+
+      it "parses empty resource" do
+        parser = BeneficiaryAddressParser.new
+
+        beneficiary_address = parser.parse(build_parser_response(attributes: {}))
+
+        expect(beneficiary_address).to be_empty
+      end
+
+      def build_parser_response(**)
+        OpenEWS::ResponseParser::ResourceParser::Resource.new(
+          id: "27",
+          type: "beneficiary_address",
+          attributes: {},
+          relationships: nil,
+          **
         )
       end
     end
